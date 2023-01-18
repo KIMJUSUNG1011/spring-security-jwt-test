c7abd9dd12110a444f237e9c85e6f30c840d4910
“김주성” <dlrtls12345@naver.com>
2023-01-18 09:49:41 +0900

    CustomUserDetailsService ê구현하다 말았음

---
+++ src/main/java/com/ssafy/jwttutorial/dto/TokenDto.java
@@ -0,0 +1,12 @@
+package com.ssafy.jwttutorial.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+    private String token;
+}
---
+++ src/main/java/com/ssafy/jwttutorial/service/CustomUserDetailsService.java
@@ -0,0 +1,46 @@
+package com.ssafy.jwttutorial.service;
+
+import com.ssafy.jwttutorial.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+// UserDetailsService 를 구현하고 UserRepository 를 주입받는 클래스
+@Component("userDetailsService")
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    // 로그인 시 DB 에서 유저 정보를 권한 정보와 함께 가지고 옴
+    public UserDetails loadUserByUsername(final String username) {
+        return userRepository.findOneWithAuthoritiesByUsername(username)
+                .map(user -> createUser(username, user))
+                .orElseThrow(() -> new UsernameNotFoundException(username + " -> 데이터베이스에서 찾을 수 없습니다."));
+    }
+
+    private org.springframework.security.core.userdetails.User createUser(String username, User user) {
+        if (!user.isActivated()) {
+            throw new RuntimeException(username + " -> 활성화되어 있지 않습니다.");
+        }
+
+        List<GrantedAuthority> grantedAuthorities = user.getAuthorities().stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.getAuthorityName()))
+                .collect(Collectors.toList());
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(),
+                user.getPassword(),
+                grantedAuthorities);
+    }
+}
